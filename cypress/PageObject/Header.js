a3711d98163c61bd7f992c7c4cfe2de33653b634
Svetlana Kanel <svetlana.kanel@gmail.com>
2023-09-19 22:53:25 -0400

    us_01.02_01

--- cypress/PageObject/Header.js
+++ cypress/PageObject/Header.js
@@ -1,6 +1,7 @@
 class Header {
     //elements
     getLogo = () => cy.get('a[href="/ref=nav_logo"]');
+    getLocation = () => cy.get('#nav-global-location-popover-link');
 
     //methods
     clickLogo(){
