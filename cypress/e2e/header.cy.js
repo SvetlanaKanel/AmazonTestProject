4f0d3c86c02b1baf16f6d89af178582f90f128f4
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2023-09-20 12:57:19 -0400

    Sk/us 01.02 001 (#3)
    * us_01.02_01
    
    * clean code
    
    * Add

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -4,13 +4,21 @@ import Header from "../PageObject/Header"
 import headerData from "../fixtures/headerData.json"
 
 describe('Tests for Header', () => {
-    const header = new Header();
-    
-    it('US_01.01_001 | Verify Logo is visible and clickable', () => {  
-        cy.visit('https://www.amazon.com/gp/cart/view.html?ref_=nav_cart');    
-        header.getLogo().should('be.visible');  
-        header.clickLogo();
-        cy.url().should('contain', headerData.logoURL);  
-      })
+  const header = new Header();
+
+  beforeEach(() => {
+    cy.visit('https://www.amazon.com/gp/cart/view.html?ref_=nav_cart');
+})
+
+  it('US_01.01_01 | Verify Logo is visible and clickable', () => {    
+    header.getLogo().should('be.visible');
+    header.clickLogo();
+    cy.url().should('contain', headerData.logoURL);
+  })
+
+  it('US_01.02_01 | Verify that location has a notification from requirements', () => {
+    header.getLocation().should('include.text', headerData.locationText);
+  })
+  
 
 })
