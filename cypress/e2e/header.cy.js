935fa15145f81edc79492887bbb83f6a1a427cb1
Svetlana Kanel <svetlana.kanel@gmail.com>
2023-09-25 21:43:27 -0400

    US_01.03_04 | select Books from Dropdown menu and Verify new page

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -4,10 +4,13 @@ import Header from "../PageObject/Header";
 import headerData from "../fixtures/headerData.json";
 import LocationPopUpWindow from "../PageObject/LocationPopUpWindow";
 import locationPopUpData from "../fixtures/locationPopUpData.json";
+import BooksPage from "../PageObject/BooksPage";
+import urlData from "../fixtures/urlData.json";
 
 describe('Tests for Header', () => {
   const header = new Header();
   const locationPopUp = new LocationPopUpWindow();
+  const books = new BooksPage();  
 
   beforeEach(() => {
     cy.visit('https://www.amazon.com/gp/cart/view.html?ref_=nav_cart');
@@ -29,24 +32,35 @@ describe('Tests for Header', () => {
   })
 
   it('US_01.03_01 | Verify that All dropdown menu is visible and has text All', () => {
-    header.getAllDropdown().should('be.visible')
+    header.getDropdownLable().should('be.visible')
       .and('have.text', headerData.AllDropdownText);
   })
 
   it('US_01.03_02 | Click on All dropdown menu and verify that dropdown list has 56 elements', () => {
-    header.clickAllDropdown();
-    header.getAllDropdownList().should('have.length', headerData.AllDroppdownList.length)
+    header.clickDropdown();
+    header.getDropdownList().should('have.length', headerData.dropdownList.length)
   })
 
-  headerData.AllDroppdownList.forEach((optionName, idx) => {
+  headerData.dropdownList.forEach((optionName, idx) => {
     it(`US_01.03_03 | Verify that option name from  All dropdown list is equal ${optionName}`, () => {
-      header.clickAllDropdown();
-      header.getAllDropdownList().eq(idx).should('include.text', headerData.AllDroppdownList[idx])
+      header.clickDropdown();
+      header.getDropdownList().eq(idx).should('include.text', headerData.dropdownList[idx])
     })
+  })
+
+  it('US_01.03_04 | Verify that the user can select a Books option from dropdown menu and get Books page', () => {
+    header.clickDropdown();
+    header.getDropdownBox().select('Books', {force: true} );
+    header.clickSearchIcon();
+
+    cy.url().should('contain', urlData.BooksPageUrlEnding);
+    books.getBooksSubnavigationBar().should('include.text', 'Books');   
   })
 
   it('US_01.04_01 | Verify that search input field placeholder is equal "Search Amazon"', () => {
     header.getSearchInputField().invoke('attr', 'placeholder').should('eq', headerData.searchInputPlaceholder);
   })
+
+
 
 })
